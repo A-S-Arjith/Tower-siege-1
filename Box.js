e0f974de3481e824737cef05aa82d47fa6c061ac
A-S-Arjith <75664994+A-S-Arjith@users.noreply.github.com>
2021-05-15 14:32:45 +0900

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,12 @@
+class Box extends BaseClass {
+constructor(x,y,width,height) {
+  super(x,y,width,height);
+}
+display(){
+  
+  var pos =this.body.position;
+  rectMode(CENTER);
+  fill("purple");
+  rect(pos.x, pos.y, this.width, this.height);
+}
+};
